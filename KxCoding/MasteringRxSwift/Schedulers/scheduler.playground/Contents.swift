ddc8ffdaf6d05562b6562b2772b2b5a629319e4e
hyemib <hyem0127@gmail.com>
2022-09-13 03:37:39 +0900

    Scheduler

--- KxCoding/MasteringRxSwift/Schedulers/scheduler.playground/Contents.swift
+++ KxCoding/MasteringRxSwift/Schedulers/scheduler.playground/Contents.swift
@@ -28,8 +28,18 @@ import RxSwift
  # Scheduler
  */
 
-let bag = DisposeBag()
+// iOS 앱을 만들때 쓰레드 처리가 필요할 경우 GCD를 사용하지만 RxSwift에서는 Scheduler를 사용.
+// Scheduler는 특정 코드가 실행되는 context를 추상화한것. context는 row level 쓰레드가 될수도 있고 Dispatch Queue나 Operation Queue가 될수도 있음.
+// Scheduler는 추상화된 context이기 때문에 쓰레드와 1대1로 매칭되지 않음. 하나의 쓰레드에 두개 이상의 개별 Scheduler가 존재하거나 하나의 Scheduler가 두개의 쓰레드에 걸쳐있는 경우도 있음.
 
+// UI를 업데이트하는 코드는 메인쓰레드에서 실행. GCD에서는 Main Queue에서 실행하고 RxSwift에서는 Main Scheduler에서 실행. 네트워크 요청이나 파일처리같은 작업을 메인쓰레드에서 실행하면 블로킹이 발생. 그래서 GCD에서 Global Queue에서 작업을 실행. RxSwift에서는 Background Scheduler를 사용.
+
+// RxSwift는 GCD와 마찬가지로 다양한 기본 Scheduler를 제공. 내부적으로 GCD와 유사한 방식으로 동작하고 실행할 작업을 스케줄링. 그리고 스케줄링 방식에 따라 Serial Scheduler와 Concurrent Scheduler로 구분. 가장 기본적은 Scheduler는 CurrentThreadScheduler[Serial Scheduler]. scheduler를 별도로 지정하지 않는다면 이 Scheduler가 사용. 메인 쓰레드와 연관된 scheduler는 MainScheduler[Serial Scheduler]. Main Queue처럼 UI를 업데이트할 때 사용. 작업을 실행할 Dispatch Queue를 직접 지정하고 싶으면 SerialDispatchQueueScheduler[Serial Scheduler]와 ConcurrentDispatchQueueScheduler[Concurrent Scheduler]를 사용. MainScheduler는 SerialDispatchQueueScheduler의 일종. Background 작업을 실행할 때는 DispatchQueueScheduler를 사용. 실행 순서를 제외하거나 동시에 실행가능한 작업 수를 제한하고 싶으면 OperationQueueScheduler[Concurrent Scheduler]를 사용. 이 scheduler는 DispatchQueue가 아닌 OperationQueue를 사용해서 생성.
+
+// 이 외에도 Unit Test에 사용하는 TestScheduler와 직접 구현할 수 있는 CustomScheduler가 제공.
+
+let bag = DisposeBag()
+/*
 Observable.of(1, 2, 3, 4, 5, 6, 7, 8, 9)
     .filter { num -> Bool in
         print(Thread.isMainThread ? "Main Thread" : "Background Thread", ">> filter")
@@ -39,3 +49,192 @@ Observable.of(1, 2, 3, 4, 5, 6, 7, 8, 9)
         print(Thread.isMainThread ? "Main Thread" : "Background Thread", ">> map")
         return num * 2
     }
+    .subscribe {
+        print(Thread.isMainThread ? "Main Thread" : "Background Thread", ">> map")
+        print($0)
+    }
+    .disposed(by: bag)
+
+/*
+ Main Thread >> filter
+ Main Thread >> filter
+ Main Thread >> map
+ Main Thread >> map
+ next(4)
+ Main Thread >> filter
+ Main Thread >> filter
+ Main Thread >> map
+ Main Thread >> map
+ next(8)
+ Main Thread >> filter
+ Main Thread >> filter
+ Main Thread >> map
+ Main Thread >> map
+ next(12)
+ Main Thread >> filter
+ Main Thread >> filter
+ Main Thread >> map
+ Main Thread >> map
+ next(16)
+ Main Thread >> filter
+ Main Thread >> map
+ completed
+ */
+*/
+
+
+
+// RxSwift에서 scheduler를 지정할 때는 observeOn(_ :)메소드와 subscribeOn(_ :)메소드를 사용.
+// observeOn(_ :)메소드는 연산자를 실행할 scheduler를 지정.
+// subscribeOn(_ :)메소드는 구독을 시작하고 종료할 때 사용할 scheduler를 지정. 구독을 시작하면 Observable에서 새로운 이벤트가 방출. 이벤트를 방출할 scheduler를 지정하는 것. 그리고 create 연산자로 구현한 코드도 subscribeOn(_ :)메소드로 지정한 scheduler에서 실행. subscribeOn(_ :)메소드를 사용하지 않는다면 subscribe 메소드가 호출된 scheduler에서 새로운 시퀀스가 시작.
+
+/*
+let backgroundScheduler = ConcurrentDispatchQueueScheduler(queue: DispatchQueue.global())
+
+Observable.of(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    .filter { num -> Bool in
+        print(Thread.isMainThread ? "Main Thread" : "Background Thread", ">> filter")
+        return num.isMultiple(of: 2)
+    }
+    .observeOn(backgroundScheduler) // map 연산자를 실행할 scheduler를 Background Scheduler로 지정.
+    .map { num -> Int in
+        
+        print(Thread.isMainThread ? "Main Thread" : "Background Thread", ">> map")
+        return num * 2
+    }
+    .subscribe {
+        print(Thread.isMainThread ? "Main Thread" : "Background Thread", ">> Subscribe")
+        print($0)
+    }
+    .disposed(by: bag)
+
+
+// map 연산자가 background에서 실행
+// observeOn 메소드는 이어지는 연산자들이 작업을 실행할 scheduler를 지정. 그래서 뒤에 있는 map은 background scheduler에서 실행되지만 앞에있는 filter에는 영향을 주지 않음.
+// subsribe에 있는 코드도 background에서 실행. observeOn 메소드로 지정한 scheduler는 다른 scheduler로 변경하기 전까지 계속 사용.
+/*
+ Main Thread >> filter
+ Main Thread >> filter
+ Main Thread >> filter
+ Main Thread >> filter
+ Background Thread >> map
+ Main Thread >> filter
+ Main Thread >> filter
+ Background Thread >> Subscribe
+ next(4)
+ Main Thread >> filter
+ Main Thread >> filter
+ Main Thread >> filter
+ Background Thread >> map
+ Background Thread >> Subscribe
+ next(8)
+ Background Thread >> map
+ Background Thread >> Subscribe
+ next(12)
+ Background Thread >> map
+ Background Thread >> Subscribe
+ next(16)
+ Background Thread >> Subscribe
+ completed
+ */
+*/
+
+/*
+let backgroundScheduler = ConcurrentDispatchQueueScheduler(queue: DispatchQueue.global())
+
+Observable.of(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    .filter { num -> Bool in
+        print(Thread.isMainThread ? "Main Thread" : "Background Thread", ">> filter")
+        return num.isMultiple(of: 2)
+    }
+    .observeOn(backgroundScheduler) // map 연산자를 실행할 scheduler를 Background Scheduler로 지정.
+    .map { num -> Int in
+        
+        print(Thread.isMainThread ? "Main Thread" : "Background Thread", ">> map")
+        return num * 2
+    }
+    .subscribeOn(MainScheduler.instance)
+    .subscribe {
+        print(Thread.isMainThread ? "Main Thread" : "Background Thread", ">> Subscribe")
+        print($0)
+    }
+    .disposed(by: bag)
+
+
+// subscribe에서 실행되는 코드는 여전히 background에서 실행되고 있음. subscribeOn 메소드는 Observable이 시작되는 시점에 어떤 scheduler를 사용할지 지정. observeOn 메소드와 달리 호출시점이 중요하지 않음.
+/*
+ Main Thread >> filter
+ Main Thread >> filter
+ Main Thread >> filter
+ Background Thread >> map
+ Background Thread >> Subscribe
+ Main Thread >> filter
+ next(4)
+ Main Thread >> filter
+ Background Thread >> map
+ Background Thread >> Subscribe
+ Main Thread >> filter
+ next(8)
+ Main Thread >> filter
+ Background Thread >> map
+ Main Thread >> filter
+ Main Thread >> filter
+ Background Thread >> Subscribe
+ next(12)
+ Background Thread >> map
+ Background Thread >> Subscribe
+ next(16)
+ Background Thread >> Subscribe
+ completed
+ */
+*/
+
+
+
+// subscribe 메소드를 메인 스레드에서 호출하고 싶은 경우
+let backgroundScheduler = ConcurrentDispatchQueueScheduler(queue: DispatchQueue.global())
+
+Observable.of(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    .subscribeOn(MainScheduler.instance)
+    .filter { num -> Bool in
+        print(Thread.isMainThread ? "Main Thread" : "Background Thread", ">> filter")
+        return num.isMultiple(of: 2)
+    }
+    .observeOn(backgroundScheduler) // map 연산자를 실행할 scheduler를 Background Scheduler로 지정.
+    .map { num -> Int in
+        
+        print(Thread.isMainThread ? "Main Thread" : "Background Thread", ">> map")
+        return num * 2
+    }
+    .observeOn(MainScheduler.instance)
+    .subscribe {
+        print(Thread.isMainThread ? "Main Thread" : "Background Thread", ">> Subscribe")
+        print($0)
+    }
+    .disposed(by: bag)
+
+/*
+ Main Thread >> filter
+ Main Thread >> filter
+ Main Thread >> filter
+ Background Thread >> map
+ Main Thread >> filter
+ Main Thread >> filter
+ Background Thread >> map
+ Main Thread >> filter
+ Main Thread >> filter
+ Main Thread >> filter
+ Background Thread >> map
+ Main Thread >> filter
+ Background Thread >> map
+ Main Thread >> Subscribe
+ next(4)
+ Main Thread >> Subscribe
+ next(8)
+ Main Thread >> Subscribe
+ next(12)
+ Main Thread >> Subscribe
+ next(16)
+ Main Thread >> Subscribe
+ completed
+ */
--- KxCoding/MasteringRxSwift/SharingSubscription/share.playground/Contents.swift
+++ KxCoding/MasteringRxSwift/SharingSubscription/share.playground/Contents.swift
@@ -31,7 +31,7 @@ import RxSwift
 // share(replay: Int = 0, SubjectLifetimeScope = .whileConnected) -> Observable<Element>
 // share 연산자는 두개의 파라미터를 받음. 첫번째 파라미터는 replay 버퍼의 크기. 파라미터로 0을 전달하면 multicast를 호출할 때 PublishSubject를 전달. 0보다 큰 값을 전달하면 ReplaySubject를 전달. 기본 값이 0으로 선언되어 있기 때문에 다른 값을 전달하지 않는다면 새로운 구독자는 구독 이후에 방출되는 이벤트만 전달받음. multicast 연산자를 호출하기 때문에 하나의 subject를 통해 시퀀스를 공유. multicast 연산자를 호출하고 refCount를 호출하므로 새로운 구독자가 추가되면 자동으로 connect되고 구독자가 더이상 없으면 disconnect.
 // 두번째 파라미터는 subject의 수명을 결정. 기본 값이 whileConnected. 새로운 구독자가 추가(connect)되면 새로운 subject가 생성. 그리고 connection이 종료되면 subject는 사라짐. forever을 전달하면 모든 connection이 하나의 subject를 공유.
-
+/*
 let bag = DisposeBag()
 let source = Observable<Int>.interval(.seconds(1), scheduler: MainScheduler.instance).debug().share()
 
@@ -55,14 +55,148 @@ DispatchQueue.main.asyncAfter(deadline: .now() + 7) {
     }
 }
 
+// share의 첫번째 파라미터의 기본 값이 0이므로 3초 뒤에 구독을 시작한 두번째 구독자는 이전에 전달된 3개의 이벤트는 받지 못함. 5초 뒤에 모든 구독이 중지되면 내부에 있는 ConnectableObservable도 중지됨. isDisposed가 출력되는데 share 연산자 내부에서 refCount 연산자를 호출하기 때문. 그리고 7초 뒤에 새로운 구독자가 추가되면 ConnectableObservable에서 새로운 시퀀스가 시작. 그래서 세번째 구독자가 처음받는 next 이벤트에는 0이 저장되어 있음.
+// share의 두번째 파라미터는 ConnectableObservable 내부에 있는 subject의 수명을 결정하는데 기본 값이 whileConnected. 새로운 구독자가 추가되면 subject를 생성하고 이어지는 구독자들을 이 subject를 공유. 그래서 첫번째 구독자와 두번째 구독자는 동일한 subject로 부터 이벤트를 받음. 두번째 구독자가 처음으로 받는 next 이벤트에 0이 아니라 3이 저장되어 있는 이유. isDisposed가 출력되는 시점에 subject는 사라지고 subscribed가 출력되는 시점에 새로운 subject가 생성. 그래서 세번째 구독자가 처음으로 받는 next 이벤트에는 0이 저장되어 있음.
+
+/*
+ 2022-09-12 23:56:14.834: share.playground:36 (__lldb_expr_11) -> subscribed
+ 2022-09-12 23:56:15.836: share.playground:36 (__lldb_expr_11) -> Event next(0)
+ 🔵 next(0)
+ 2022-09-12 23:56:16.835: share.playground:36 (__lldb_expr_11) -> Event next(1)
+ 🔵 next(1)
+ 2022-09-12 23:56:17.836: share.playground:36 (__lldb_expr_11) -> Event next(2)
+ 🔵 next(2)
+ 2022-09-12 23:56:18.836: share.playground:36 (__lldb_expr_11) -> Event next(3)
+ 🔵 next(3)
+ 🔴 next(3)
+ 2022-09-12 23:56:19.835: share.playground:36 (__lldb_expr_11) -> Event next(4)
+ 🔵 next(4)
+ 🔴 next(4)
+ 2022-09-12 23:56:20.357: share.playground:36 (__lldb_expr_11) -> isDisposed
+ 2022-09-12 23:56:22.557: share.playground:36 (__lldb_expr_11) -> subscribed
+ 2022-09-12 23:56:23.559: share.playground:36 (__lldb_expr_11) -> Event next(0)
+ ⚫️ next(0)
+ 2022-09-12 23:56:24.559: share.playground:36 (__lldb_expr_11) -> Event next(1)
+ ⚫️ next(1)
+ 2022-09-12 23:56:25.559: share.playground:36 (__lldb_expr_11) -> Event next(2)
+ ⚫️ next(2)
+ 2022-09-12 23:56:25.857: share.playground:36 (__lldb_expr_11) -> isDisposed
+ */
+*/
 
 
 
 
+/*
+let bag = DisposeBag()
+let source = Observable<Int>.interval(.seconds(1), scheduler: MainScheduler.instance).debug().share(replay: 5)
+
+let observer1 = source
+    .subscribe { print("🔵", $0) }
+
+let observer2 = source
+    .delaySubscription(.seconds(3), scheduler: MainScheduler.instance)
+    .subscribe { print("🔴", $0) }
+
+DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+    observer1.dispose()
+    observer2.dispose()
+}
+
+DispatchQueue.main.asyncAfter(deadline: .now() + 7) {
+    let observer3 = source.subscribe { print("⚫️", $0) }
+    
+    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+        observer3.dispose()
+    }
+}
+
+// 새로운 구독자는 구독이 시작되는 시점에 버퍼에 저장되어 있는 이벤트를 함께 전달받음. 그래서 두번째 구독자는 이전에 전달되었던 next 이벤트도 함께 전달받음. 하지만 세번째 구독자는 새로운 subject로부터 이벤트를 전달받기 때문에 구독시점에 하나의 next 이벤트만 받음.
+/*
+ 2022-09-13 01:10:35.838: share.playground:92 (__lldb_expr_13) -> subscribed
+ 2022-09-13 01:10:36.848: share.playground:92 (__lldb_expr_13) -> Event next(0)
+ 🔵 next(0)
+ 2022-09-13 01:10:37.848: share.playground:92 (__lldb_expr_13) -> Event next(1)
+ 🔵 next(1)
+ 2022-09-13 01:10:38.848: share.playground:92 (__lldb_expr_13) -> Event next(2)
+ 🔵 next(2)
+ 🔴 next(0)
+ 🔴 next(1)
+ 🔴 next(2)
+ 2022-09-13 01:10:39.848: share.playground:92 (__lldb_expr_13) -> Event next(3)
+ 🔵 next(3)
+ 🔴 next(3)
+ 2022-09-13 01:10:40.848: share.playground:92 (__lldb_expr_13) -> Event next(4)
+ 🔵 next(4)
+ 🔴 next(4)
+ 2022-09-13 01:10:41.354: share.playground:92 (__lldb_expr_13) -> isDisposed
+ 2022-09-13 01:10:43.555: share.playground:92 (__lldb_expr_13) -> subscribed
+ 2022-09-13 01:10:44.558: share.playground:92 (__lldb_expr_13) -> Event next(0)
+ ⚫️ next(0)
+ 2022-09-13 01:10:45.558: share.playground:92 (__lldb_expr_13) -> Event next(1)
+ ⚫️ next(1)
+ 2022-09-13 01:10:46.557: share.playground:92 (__lldb_expr_13) -> Event next(2)
+ ⚫️ next(2)
+ 2022-09-13 01:10:46.860: share.playground:92 (__lldb_expr_13) -> isDisposed
+ */
+*/
 
 
 
+let bag = DisposeBag()
+let source = Observable<Int>.interval(.seconds(1), scheduler: MainScheduler.instance).debug().share(replay: 5, scope: .forever) // 모든 구독자가 하나의 subject를 공유.
 
+let observer1 = source
+    .subscribe { print("🔵", $0) }
 
+let observer2 = source
+    .delaySubscription(.seconds(3), scheduler: MainScheduler.instance)
+    .subscribe { print("🔴", $0) }
 
+DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+    observer1.dispose()
+    observer2.dispose()
+}
 
+DispatchQueue.main.asyncAfter(deadline: .now() + 7) {
+    let observer3 = source.subscribe { print("⚫️", $0) }
+    
+    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+        observer3.dispose()
+    }
+}
+
+// 세번째 구독자가 추가되는 시점에 버퍼에 저장되어 있는 5개의 이벤트가 함께 전달.
+// 이어지는 next 이벤트에 5가 아닌 0이 저장되어 있음 -> 시퀀스가 중지된 다음에 새로운 구독자가 추가되면 새로운 시퀀스가 시작되기 때문. 
+/*
+ 2022-09-13 01:15:21.920: share.playground:147 (__lldb_expr_15) -> subscribed
+ 2022-09-13 01:15:22.922: share.playground:147 (__lldb_expr_15) -> Event next(0)
+ 🔵 next(0)
+ 2022-09-13 01:15:23.922: share.playground:147 (__lldb_expr_15) -> Event next(1)
+ 🔵 next(1)
+ 2022-09-13 01:15:24.922: share.playground:147 (__lldb_expr_15) -> Event next(2)
+ 🔵 next(2)
+ 🔴 next(0)
+ 🔴 next(1)
+ 🔴 next(2)
+ 2022-09-13 01:15:25.922: share.playground:147 (__lldb_expr_15) -> Event next(3)
+ 🔵 next(3)
+ 🔴 next(3)
+ 2022-09-13 01:15:26.922: share.playground:147 (__lldb_expr_15) -> Event next(4)
+ 🔵 next(4)
+ 🔴 next(4)
+ 2022-09-13 01:15:27.447: share.playground:147 (__lldb_expr_15) -> isDisposed
+ ⚫️ next(0)
+ ⚫️ next(1)
+ ⚫️ next(2)
+ ⚫️ next(3)
+ ⚫️ next(4)
+ 2022-09-13 01:15:29.645: share.playground:147 (__lldb_expr_15) -> subscribed
+ 2022-09-13 01:15:30.647: share.playground:147 (__lldb_expr_15) -> Event next(0)
+ ⚫️ next(0)
+ 2022-09-13 01:15:31.647: share.playground:147 (__lldb_expr_15) -> Event next(1)
+ ⚫️ next(1)
+ 2022-09-13 01:15:32.647: share.playground:147 (__lldb_expr_15) -> Event next(2)
+ ⚫️ next(2)
+ 2022-09-13 01:15:32.946: share.playground:147 (__lldb_expr_15) -> isDisposed
+ */
